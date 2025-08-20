6a113d595b2fb8540d18e51506ef1300dedba80c
stdrouuu <brajs0111@gmail.com>
2025-08-20 10:25:13 +0700

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Building a server using Hapi");
+// Building simple Hapi.js web Server
+// npm install @hapi/hapi
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log(`Server is running at ${server.info.uri}`);
+};
+
+init();
+// to see response, run terminal command:
+// curl -X GET http://localhost:5000  ==> Not Found (Hapi default response if theres no event handler)
