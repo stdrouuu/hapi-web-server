e7e4b7ed08edfe396a231c9099d423771612669c
stdrouuu <brajs0111@gmail.com>
2025-08-20 11:05:42 +0700

    path parameter

--- routes.js
+++ routes.js
@@ -1,4 +1,45 @@
 // 2. Routing
+// const routes = [
+//   {
+//     method: "GET",
+//     path: "/",
+//     handler: (request, h) => {
+//       return "Homepage";
+//     },
+//   },
+//   {
+//     method: "*",
+//     path: "/",
+//     handler: (request, h) => {
+//       return "Halaman tidak dapat diakses dengan method tersebut";
+//     },
+//   },
+//   {
+//     method: "GET",
+//     path: "/about",
+//     handler: (request, h) => {
+//       return "About page";
+//     },
+//   },
+//   {
+//     method: "*",
+//     path: "/about",
+//     handler: (request, h) => {
+//       return "Halaman tidak dapat diakses dengan method";
+//     },
+//   },
+//   {
+//     method: "*",
+//     path: "/{any*}",
+//     handler: (request, h) => {
+//       return "Halaman tidak ditemukan";
+//     },
+//   },
+// ];
+
+// module.exports = routes;
+
+// 3. Path Parameter
 const routes = [
   {
     method: "GET",
@@ -26,6 +67,14 @@ const routes = [
     path: "/about",
     handler: (request, h) => {
       return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      return `Hello, ${name}!`;
     },
   },
   {
--- server.js
+++ server.js
@@ -18,6 +18,32 @@
 
 
 // 2. Method/Verb Request and Routing
+// const Hapi = require("@hapi/hapi");
+// const routes = require("./routes");
+
+// const init = async () => {
+//   const server = Hapi.server({
+//     port: 5000,
+//     host: "localhost",
+//   });
+
+//   server.route(routes); //pake server.route buat routes.js
+
+//   await server.start();
+//   console.log(`Server is running at ${server.info.uri}`);
+// };
+
+// init();
+// to see response, run in terminal/cmd:
+// curl -X GET http://localhost:5000 ==> output: Homepage
+// curl -X GET http://localhost:5000/about ==> output: About page
+// curl -X GET http://localhost:5000/test ==> utput: Halaman tidak ditemukan
+// curl -X POST http://localhost:5000 ==> output: Halaman tidak dapat diakses dengan method tersebut
+
+
+// 3. Path Parameter
+// https://twitter.com/maudyayunda ==> username = maudyayunda
+// di Hapi, pake {} di server.route
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
@@ -30,12 +56,10 @@ const init = async () => {
   server.route(routes); //pake server.route buat routes.js
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server is running at ${server.info.uri}`);
 };
 
 init();
 // to see response, run in terminal/cmd:
-// curl -X GET http://localhost:5000 ==> output: Homepage
-// curl -X GET http://localhost:5000/about ==> output: About page
-// curl -X GET http://localhost:5000/test ==> utput: Halaman tidak ditemukan
-// curl -X POST http://localhost:5000 ==> output: Halaman tidak dapat diakses dengan method tersebut
+// curl -X GET http://localhost:5000/hello/brandon ==> output: Hello, brandon!
+// curl -X GET http://localhost:5000/hello ==> output: Hello, stranger!
